42444eb234739708908acd224258c84f0559f816
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-22 15:10:48 +0200

    Add utility make files

---
+++ cpp-utils.mk
@@ -0,0 +1,45 @@
+# Create rules to compile each cpp file of a folder
+
+define folder_compile = 
+
+debug/src$(1)/%.cpp.o: src$(1)/%.cpp
+	@ mkdir -p debug/src$(1)/
+	$(CXX) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $$@ -c $$<
+
+release/src$(1)/%.cpp.o: src$(1)/%.cpp
+	@ mkdir -p release/src$(1)/
+	$(CXX) $(CXX_FLAGS) $(RELEASE_FLAGS) -o $$@ -c $$<
+
+debug/src$(1)/%.cpp.d: $(CPP_FILES)
+	@ mkdir -p debug/src$(1)/
+	@ $(CXX) $(CXX_FLAGS) $(DEBUG_FLAGS) -MM -MT debug/src$(1)/$$*.cpp.o src$(1)/$$*.cpp | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $$@
+
+release/src$(1)/%.cpp.d: $(CPP_FILES)
+	@ mkdir -p release/src$(1)/
+	@ $(CXX) $(CXX_FLAGS) $(RELEASE_FLAGS) -MM -MT release/src$(1)/$$*.cpp.o src$(1)/$$*.cpp | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $$@
+
+endef
+
+# Create rules to link an executable with a set of files
+
+define add_executable =
+
+debug/bin/$(1): $(addsuffix .o,$(addprefix debug/src/,$(2)))
+	@ mkdir -p debug/bin/
+	$(LD) $(LD_FLAGS) $(3) $(DEBUG_FLAGS) -o $$@ $$+
+
+release/bin/$(1): $(addsuffix .o,$(addprefix release/src/,$(2)))
+	@ mkdir -p release/bin/
+	$(LD) $(LD_FLAGS) $(3) $(RELEASE_FLAGS) -o $$@ $$+
+
+endef
+
+# Create executable sets targets
+
+define add_executable_set =
+
+release_$(1): $(addprefix release/bin/,$(2))
+debug_$(1): $(addprefix debug/bin/,$(2))
+$(1): release_$(1)
+
+endef
