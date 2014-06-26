7ebdf28a2a4c2691f77cc9dffbf087dba7e51215
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-26 13:14:52 +0200

    Add support for test executables

--- cpp-utils.mk
+++ cpp-utils.mk
@@ -1,6 +1,6 @@
 # Create rules to compile each cpp file of a folder
 
-define folder_compile = 
+define folder_compile =
 
 debug/src$(1)/%.cpp.o: src$(1)/%.cpp
 	@ mkdir -p debug/src$(1)/
@@ -20,6 +20,26 @@ release/src$(1)/%.cpp.d: $(CPP_FILES)
 
 endef
 
+define test_folder_compile =
+
+debug/test$(1)/%.cpp.o: test$(1)/%.cpp
+	@ mkdir -p debug/test$(1)/
+	$(CXX) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $$@ -c $$<
+
+release/test$(1)/%.cpp.o: test$(1)/%.cpp
+	@ mkdir -p release/test$(1)/
+	$(CXX) $(CXX_FLAGS) $(RELEASE_FLAGS) -o $$@ -c $$<
+
+debug/test$(1)/%.cpp.d: $(CPP_FILES)
+	@ mkdir -p debug/test$(1)/
+	@ $(CXX) $(CXX_FLAGS) $(DEBUG_FLAGS) -MM -MT debug/test$(1)/$$*.cpp.o test$(1)/$$*.cpp | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $$@
+
+release/test$(1)/%.cpp.d: $(CPP_FILES)
+	@ mkdir -p release/test$(1)/
+	@ $(CXX) $(CXX_FLAGS) $(RELEASE_FLAGS) -MM -MT release/test$(1)/$$*.cpp.o test$(1)/$$*.cpp | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@' > $$@
+
+endef
+
 # Create rules to link an executable with a set of files
 
 define add_executable =
@@ -29,6 +49,18 @@ debug/bin/$(1): $(addsuffix .o,$(addprefix debug/src/,$(2)))
 	$(LD) $(LD_FLAGS) $(3) $(DEBUG_FLAGS) -o $$@ $$+
 
 release/bin/$(1): $(addsuffix .o,$(addprefix release/src/,$(2)))
+	@ mkdir -p release/bin/
+	$(LD) $(LD_FLAGS) $(3) $(RELEASE_FLAGS) -o $$@ $$+
+
+endef
+
+define add_test_executable =
+
+debug/bin/$(1): $(addsuffix .o,$(addprefix debug/test/,$(2)))
+	@ mkdir -p debug/bin/
+	$(LD) $(LD_FLAGS) $(3) $(DEBUG_FLAGS) -o $$@ $$+
+
+release/bin/$(1): $(addsuffix .o,$(addprefix release/test/,$(2)))
 	@ mkdir -p release/bin/
 	$(LD) $(LD_FLAGS) $(3) $(RELEASE_FLAGS) -o $$@ $$+
 
