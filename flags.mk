94b110b4a693a79f325a2627ffcc90e4487907e4
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-26 16:19:24 +0200

    Fix

--- flags.mk
+++ flags.mk
@@ -1,7 +1,7 @@
 CXX=clang++
 LD=clang++
 
-ifeq($(CXX),clang++)
+ifeq ($(CXX),clang++)
 	WARNING_FLAGS=-Wextra -Wall -Qunused-arguments -Wuninitialized -Wsometimes-uninitialized -Wno-long-long -Winit-self -Wdocumentation
 else
 	WARNING_FLAGS=-Wextra -Wall -Wuninitialized -Wno-long-long -Winit-self
@@ -12,7 +12,7 @@ LD_FLAGS=$(CXX_FLAGS)
 
 DEBUG_FLAGS=-g
 
-ifeq($(CXX),clang++)
+ifeq ($(CXX),clang++)
 	RELEASE_FLAGS=-g -DNDEBUG -Ofast -march=native -fvectorize -fslp-vectorize-aggressive -fomit-frame-pointer
 else
 	RELEASE_FLAGS=-g -DNDEBUG -Ofast -march=native -fomit-frame-pointer
