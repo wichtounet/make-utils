969f2b1873ddafeb4b3a793390c07680a9fce349
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-26 16:13:14 +0200

    By default no library should be included

--- flags.mk
+++ flags.mk
@@ -3,7 +3,7 @@ LD=clang++
 
 WARNING_FLAGS=-Wextra -Wall -Qunused-arguments -Wuninitialized -Wsometimes-uninitialized -Wno-long-long -Winit-self -Wdocumentation
 CXX_FLAGS=-Iinclude -std=c++1y $(WARNING_FLAGS)
-LD_FLAGS=$(CXX_FLAGS) -luuid -lboost_system -lboost_filesystem -lboost_date_time
+LD_FLAGS=$(CXX_FLAGS) 
 
 DEBUG_FLAGS=-g
 RELEASE_FLAGS=-g -DNDEBUG -Ofast -march=native -fvectorize -fslp-vectorize-aggressive -fomit-frame-pointer
