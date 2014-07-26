8572b92cb37dfe5d3382d4eba7a13cc8af8aaad0
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-26 16:17:18 +0200

    Improve flags

--- flags.mk
+++ flags.mk
@@ -1,9 +1,19 @@
 CXX=clang++
 LD=clang++
 
-WARNING_FLAGS=-Wextra -Wall -Qunused-arguments -Wuninitialized -Wsometimes-uninitialized -Wno-long-long -Winit-self -Wdocumentation
+ifeq($(CXX),clang++)
+	WARNING_FLAGS=-Wextra -Wall -Qunused-arguments -Wuninitialized -Wsometimes-uninitialized -Wno-long-long -Winit-self -Wdocumentation
+else
+	WARNING_FLAGS=-Wextra -Wall -Wuninitialized -Wno-long-long -Winit-self
+endif
+
 CXX_FLAGS=-Iinclude -std=c++1y $(WARNING_FLAGS)
 LD_FLAGS=$(CXX_FLAGS) 
 
 DEBUG_FLAGS=-g
-RELEASE_FLAGS=-g -DNDEBUG -Ofast -march=native -fvectorize -fslp-vectorize-aggressive -fomit-frame-pointer
+
+ifeq($(CXX),clang++)
+	RELEASE_FLAGS=-g -DNDEBUG -Ofast -march=native -fvectorize -fslp-vectorize-aggressive -fomit-frame-pointer
+else
+	RELEASE_FLAGS=-g -DNDEBUG -Ofast -march=native -fomit-frame-pointer
+endif
