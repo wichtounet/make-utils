3e908956d3cf5d7f6d521e6e9bd695549f8e8deb
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-14 11:36:16 +0200

    Better condition for clang

--- flags.mk
+++ flags.mk
@@ -1,18 +1,18 @@
 CXX=clang++
 LD=clang++
 
-ifeq ($(CXX),clang++)
+ifneq (,$(findstring,clang,$(CXX)))
 	WARNING_FLAGS=-Wextra -Wall -Qunused-arguments -Wuninitialized -Wsometimes-uninitialized -Wno-long-long -Winit-self -Wdocumentation
 else
 	WARNING_FLAGS=-Wextra -Wall -Wuninitialized -Wno-long-long -Winit-self
 endif
 
 CXX_FLAGS=-Iinclude -std=c++1y $(WARNING_FLAGS)
-LD_FLAGS=$(CXX_FLAGS) 
+LD_FLAGS=$(CXX_FLAGS)
 
 DEBUG_FLAGS=-g
 
-ifeq ($(CXX),clang++)
+ifneq (,$(findstring,clang,$(CXX)))
 	RELEASE_FLAGS=-g -DNDEBUG -O3 -march=native -fvectorize -fslp-vectorize-aggressive -fomit-frame-pointer
 else
 	RELEASE_FLAGS=-g -DNDEBUG -O3 -march=native -fomit-frame-pointer
