86021f077ee2360ae9cc36291897a3368cbc4d0b
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-15 08:23:12 +0200

    Fix conditionals

--- flags.mk
+++ flags.mk
@@ -1,7 +1,7 @@
 CXX=clang++
 LD=clang++
 
-ifneq (,$(findstring,clang,$(CXX)))
+ifneq (,$(findstring clang,$(CXX)))
 	WARNING_FLAGS=-Wextra -Wall -Qunused-arguments -Wuninitialized -Wsometimes-uninitialized -Wno-long-long -Winit-self -Wdocumentation
 else
 	WARNING_FLAGS=-Wextra -Wall -Wuninitialized -Wno-long-long -Winit-self
@@ -12,13 +12,13 @@ LD_FLAGS=$(CXX_FLAGS)
 
 DEBUG_FLAGS=-g
 
-ifneq (,$(findstring,clang,$(CXX)))
+ifneq (,$(findstring clang,$(CXX)))
 	RELEASE_FLAGS=-g -DNDEBUG -O3 -fvectorize -fslp-vectorize-aggressive -fomit-frame-pointer
 else
 	RELEASE_FLAGS=-g -DNDEBUG -O3 -fomit-frame-pointer
 endif
 
 # If we do not compile with distcc, enable march=native
-ifeq (,$(findstring,distcc,$(CXX)))
+ifeq (,$(findstring distcc,$(CXX)))
 	RELEASE_FLAGS += -march=native
 endif
