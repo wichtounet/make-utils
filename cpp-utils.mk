5f70a1dac6d2b22214e61f19c7563f0aecee935e
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-11-03 12:03:24 +0100

    Make it compatible with make 3.81

--- cpp-utils.mk
+++ cpp-utils.mk
@@ -2,7 +2,7 @@
 
 AUTO_SRC_FILES=
 
-define folder_compile =
+define folder_compile
 
 debug/$(1)/%.cpp.o: $(1)/%.cpp
 	@ mkdir -p debug/$(1)/
@@ -22,19 +22,19 @@ release/$(1)/%.cpp.d: $(CPP_FILES)
 
 endef
 
-define src_folder_compile =
+define src_folder_compile
 
 $(eval $(call folder_compile,src$(1),$(2)))
 
 endef
 
-define test_folder_compile =
+define test_folder_compile
 
 $(eval $(call folder_compile,test$(1),$(2)))
 
 endef
 
-define auto_folder_compile =
+define auto_folder_compile
 
 $(eval $(call folder_compile,$(1),$(2)))
 
@@ -44,7 +44,7 @@ endef
 
 # Create rules to link an executable with a set of files
 
-define add_executable =
+define add_executable
 
 debug/bin/$(1): $(addsuffix .o,$(addprefix debug/,$(2)))
 	@ mkdir -p debug/bin/
@@ -56,25 +56,25 @@ release/bin/$(1): $(addsuffix .o,$(addprefix release/,$(2)))
 
 endef
 
-define add_src_executable =
+define add_src_executable
 
 $(eval $(call add_executable,$(1),$(addprefix src/,$(2)),$(3)))
 
 endef
 
-define add_test_executable =
+define add_test_executable
 
 $(eval $(call add_executable,$(1),$(addprefix test/,$(2)),$(3)))
 
 endef
 
-define add_auto_src_executable =
+define add_auto_src_executable
 
 $(eval $(call add_executable,$(1),$(wildcard src/*.cpp)))
 
 endef
 
-define auto_add_executable =
+define auto_add_executable
 
 $(eval $(call add_executable,$(1),$(AUTO_SRC_FILES)))
 $(eval $(call add_executable_set,$(1),$(1)))
@@ -83,7 +83,7 @@ endef
 
 # Create executable sets targets
 
-define add_executable_set =
+define add_executable_set
 
 release_$(1): $(addprefix release/bin/,$(2))
 debug_$(1): $(addprefix debug/bin/,$(2))
@@ -93,7 +93,7 @@ endef
 
 # Include D files
 
-define auto_finalize =
+define auto_finalize
 
 AUTO_DEBUG_D_FILES=$(AUTO_SRC_FILES:%.cpp=debug/%.cpp.d)
 AUTO_RELEASE_D_FILES=$(AUTO_SRC_FILES:%.cpp=release/%.cpp.d)
